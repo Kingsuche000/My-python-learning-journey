bd3469161b296d99ef526ddd6b88f3fe2ee64df0
Kingsley UD <Kingsuche000@gmail.com>
2026-06-23 01:50:05 +0100

    Create 01_variables_data_types_lists.py
    A few examples to match my first lesson

---
+++ 01_variables_data_types_lists.py
@@ -0,0 +1,61 @@
+# ==========================
+# VARIABLES
+# ==========================
+
+cancer_gene = "BRCA1"
+gene = "P53"
+
+# ==========================
+# INTEGER
+# ==========================
+
+number_of_dna_bases = 4
+
+# ==========================
+# FLOAT
+# ==========================
+
+fossil_weight = 40.28
+
+# ==========================
+# STRING
+# ==========================
+
+dna = "A molecule composed of nucleotide bases and a sugar-phosphate backbone"
+
+# ==========================
+# BOOLEAN
+# ==========================
+
+dna_is_helix_shaped = True
+
+# ==========================
+# LIST
+# ==========================
+
+cell_types = ["T Cells", "B Cells", "Red Blood Cells", "NK Cells"]
+
+# Add an item to the list
+
+cell_types.append("Macrophages")
+
+# Replace an item in the list
+
+cell_types[1] = "Memory B Cells"
+
+# Print the entire list
+
+print(cell_types)
+
+# Print specific items
+
+print(cell_types[0])
+print(cell_types[2])
+
+# ==========================
+# TUPLE
+# ==========================
+
+nucleotide_bases = ("Adenine", "Thymine", "Cytosine", "Guanine")
+
+print(nucleotide_bases)
