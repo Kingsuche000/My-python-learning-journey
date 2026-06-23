b0e0ec5f5143c5dafffb1cb3e4e623bcb5637534
Kingsley UD <Kingsuche000@gmail.com>
2026-06-23 20:30:14 +0100

    Create 01_variables_data_types_lists_exercise.py
    A small exercise - immune cell tracker

---
+++ 01_variables_data_types_lists_exercise.py
@@ -0,0 +1,8 @@
+cell_types = ["T Cells", "B Cells", "NK Cells"]
+cell_types.append("Macrophages")
+cell_types.append("Dendritic Cells")
+cell_types[1] = "Memory B Cells"
+print(cell_types)
+print(cell_types[0])
+print(cell_types[2])
+print(cell_types[4])
